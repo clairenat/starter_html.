2d1410cb5823b81f75cb6ff0062c53056441e190
clairenat <claireca@umich.edu>
2023-11-03 15:16:59 -0400

    added read more and read less buttons and currently working on changing the hero section, also need to toggle with the gifs on the travel page

---
+++ js/javascriptstyle.js
@@ -0,0 +1,120 @@
+function myFunction() {
+    let showntext = document.getElementById("shown");
+    let moreText = document.getElementById("more");
+    let buttonText = document.getElementById("MyButton");
+    
+    if (showntext.style.display === "none") {
+        showntext.style.display = "inline";
+        buttonText.innerHTML = "Read more";
+        moreText.style.display = "none";
+        console.log("In read more")
+      } else {
+        showntext.style.display = "none";
+        buttonText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+        console.log("In read less")
+      }
+    //if (showntext.style.display === "none") {
+      //showntext.style.display = "inline";
+      //buttonText.innerHTML = "Read more";
+      //moreText.style.display = "none";
+    //} else {
+    //  showntext.style.display = "none";
+    //  buttonText.innerHTML = "Read less";
+    //  moreText.style.display = "inline";
+    //}
+  }
+
+//function myFunction2() {
+    //let button2text = document.getElementById('MyButton2')
+    //let showntext = document.getElementById("shown");
+    //let moreText = document.getElementById("more");
+    //if (shownText.style.display != "none"){
+    //    button2text.style.display = "inline";
+    //}
+//}
+function myFunction2() {
+    let showntext = document.getElementById("shown2");
+    let moreText = document.getElementById("more2");
+    let buttonText = document.getElementById("MyButton2");
+    
+    if (showntext.style.display === "none") {
+        showntext.style.display = "inline";
+        buttonText.innerHTML = "Read more";
+        moreText.style.display = "none";
+        console.log("In read more")
+      } else {
+        showntext.style.display = "none";
+        buttonText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+        console.log("In read less")
+      }
+    }
+function myFunction3() {
+    let showntext = document.getElementById("shown3");
+    let moreText = document.getElementById("more3");
+    let buttonText = document.getElementById("MyButton3");
+    
+    if (showntext.style.display === "none") {
+        showntext.style.display = "inline";
+        buttonText.innerHTML = "Read more";
+        moreText.style.display = "none";
+        console.log("In read more")
+      } else {
+        showntext.style.display = "none";
+        buttonText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+        console.log("In read less")
+      }
+    }
+function myFunction4() {
+    let showntext = document.getElementById("shown4");
+    let moreText = document.getElementById("more4");
+    let buttonText = document.getElementById("MyButton4");
+    
+    if (showntext.style.display === "none") {
+        showntext.style.display = "inline";
+        buttonText.innerHTML = "Read more";
+        moreText.style.display = "none";
+        console.log("In read more")
+      } else {
+        showntext.style.display = "none";
+        buttonText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+        console.log("In read less")
+      }
+    }
+function myFunction5() {
+    let showntext = document.getElementById("shown5");
+    let moreText = document.getElementById("more5");
+    let buttonText = document.getElementById("MyButton5");
+    
+    if (showntext.style.display === "none") {
+        showntext.style.display = "inline";
+        buttonText.innerHTML = "Read more";
+        moreText.style.display = "none";
+        console.log("In read more")
+      } else {
+        showntext.style.display = "none";
+        buttonText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+        console.log("In read less")
+      }
+    }
+function myFunction6() {
+    let showntext = document.getElementById("shown6");
+    let moreText = document.getElementById("more6");
+    let buttonText = document.getElementById("MyButton6");
+    
+    if (showntext.style.display === "none") {
+        showntext.style.display = "inline";
+        buttonText.innerHTML = "Read more";
+        moreText.style.display = "none";
+        console.log("In read more")
+      } else {
+        showntext.style.display = "none";
+        buttonText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+        console.log("In read less")
+      }
+    }
